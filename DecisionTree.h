8e899b97f2e1a3691f548919dec6a0912afeadf1
wangrn <wang_rn@163.com>
2013-07-26 16:10:52 +0800

    Update DecisionTree.h

--- DecisionTree.h
+++ DecisionTree.h
@@ -1,9 +1,39 @@
 #ifndef DECISION_TREE_H
 #define DECISION_TREE_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <map>
+#include <set>
+using namespace std;
+
+#include "MinHeap.h"
+
+
 namespace mlf {
 namespace dt {
 
+class knot
+{
+public:
+    float value;
+    int   label;
 }
+
+class ig_split
+{
+public:
+    utils::min_heap<knot>* mh;
+
+public:
+    ig_split() {
+        mh = new utils::min_heap<knot>();
+    }
+    void setup(int max_feature) {
+        mh->init(max_feature);
+    }
+}
+
+};
 };
 #endif
