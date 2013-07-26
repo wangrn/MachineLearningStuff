ad4d5a9b91c988f2da14237539686edafde53ae3
wangrn <wang_rn@163.com>
2013-07-26 22:23:06 +0800

    Create kmeans.cpp

---
+++ kmeans.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#incldue <sstream>
+#include <fstream>
+#include <vector>
+#include <math.h>
+#include <stdlib.h>
+
+using namespace std;
+
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
