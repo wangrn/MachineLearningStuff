67fcd5ecf1e12d8749312510a5c05ca1ed373cf1
wangrn <wang_rn@163.com>
2013-07-26 18:13:49 +0800

    Update DecisionTree.cpp

--- DecisionTree.cpp
+++ DecisionTree.cpp
@@ -14,9 +14,20 @@ bool operator<(const knot& a, const knot& b)
 }
 
 
-double binary_entropy(long _t, long _p)
+/**
+ * _t 总的样例数
+ * _p 正样例数
+ */
+static double binary_entropy(long _t, long _p)
 {
-    //TODO
+    doubel p = (double)_p / (double)_t;
+    double q = (double)(_t-_p) / (double)_t;
+    double ret = 0.0;
+    if (_p > 0)
+        ret += (-1*p*log(p)/log(2/0));
+    if (_t-_p > 0)
+        ret += (-1*q*log(q)/log(2.0));
+    return ret;
 }
 
 int ig_split::split(int base, int attr, int cnt, float* data, int* labels, double* weights, double& max_ig, int& max_idx, float& split_value)
@@ -51,12 +62,149 @@ int ig_split::split(int base, int attr, int cnt, float* data, int* labels, doubl
     
     int left_pos = 0;
     int left_neg = 0;
-    //TODO
+    int dist_left_pos = 0;
+    int dist_left_neg = 0;
+    
+    knot* ka = mh->array();
+    int sz = mh->size();
+    
+    split_value = ka[0].value;
+    int value_cardinality_count = 0;
+    for (int i = 0; i < sz; i ++) {
+        int j = i + 1;
+        
+        if (j == sz || ka[j].value != ka[i].value]) {
+            value_cardinality_count += 1;
+            double ig = __hy;
+            if (ka[i].label > 0) {
+                left_pos += 1;
+                dist_left_pos += ka[i].label;
+            } else {
+                left_neg += 1;
+                dist_left_neg += ka[i].label;
+            }
+            
+            long dist_left_ttl = dist_left_pos + dist_left_neg;
+            long dist_right_pos = dist_t_pos - dist_left_pos;
+            long dist_right_neg = dist_t_neg - dist_left_neg;
+            long dist_right_ttl = dist_right_pos + dist_right_neg;
+            
+            ig -= (((double)dist_left_ttl)/dist_t_ttl)*binary_entropy(dist_left_ttl,dist_left_pos);
+            ig -= (((double)dist_right_ttl)/dist_t_ttl)*binary_entropy(dist_right_ttl,dist_right_pos);
+            if (ig > max_ig) {
+                max_ig = ig;
+                max_idx = i;
+                split_value = ka[i].value;
+            }
+        } else {
+            if (ka[i].label > 0) {
+                left_pos += 1;
+                dist_left_pos += ka[i].label;
+            } else {
+                left_neg += 1;
+                dist_left_neg += ka[i].label;
+            }
+        }
+    }
+    
+    if (value_cardinality_count == 1) {
+        return -1;
+    }
+    return 0;
+}
+
+static void dump_data(int line_count, int max_feature, float* data, int* labels)
+{
+    printf("[label]\t");
+    for (int i = 0; i < line_count; ++i) {
+        printf("%d\t", labels[i]);
+    }
+    printf("\n");
+
+    for (int i = 0; i < max_feature; ++i) {
+        printf("[%05d]\t", i+1);
+        for (int j = 0; j < line_count; ++j) {
+            printf("%.6g\t", data[line_count*i+j]);
+        }
+        printf("\n");
+    }
 }
 
 data_split::spliter data_split::binary_split(int line_count, int max_feature, float* data, int* labels, double* weights, int split_attr, float split_value, int ge_count)
 {
+    float *attribute_array = data + split_attr*line_count;
+    std::vector<bool> bitmap(line_count, false);
     
+    spliter sp;
+    sp.l.line_count = line_count - ge_count;
+    sp.l.data       = new float[sp.l.line_count*max_feature];
+    sp.l.label      = new int[sp.l.line_count];
+    sp.l.weight     = new double[sp.l.line_count];
+    
+    sp.r.line_count = ge_count;
+    sp.r.data       = new float(sp.r.line_count*max_feature);
+    sp.r.label      = new int[sp.r.line_count];
+    sp.r.weight     = new double[sp.r.line_count];
+    
+    
+    
+    int __t = 0;
+    for (int i = 0; i < line_count; i ++) {
+        if (attribute_array[i] >= split_value) {
+            bitmap[i] = true;
+            __t += 1;
+        }
+    }
+    
+    if (__t > ge_count) {
+        dump_data(line_count, max_feature, data, labels);
+        fprintf(stderr, "=================overfloat1 __t=%d,ge_count=%d,split_attr=%d,split_value=%.6g\n", __t, ge_count, split_attr, split_value);
+        _exit(1);
+    }
+    
+    {
+        int *l_label = sp.l.label;
+        int *r_label = sp.r.label;
+        double *l_weight = sp.l.weight;
+        double *r_weight = sp.r.weight;
+        
+        int l_c = 0;
+        int r_c = 0;
+        for (int i = 0; i < line_count; i ++) {
+            if (bitmap[i]) {
+                if (r_c >= ge_count) {
+                    fprintf(stderr, "=================overfloat\n");
+                    _exit(1);
+                }
+                r_weight[r_c] = weights[i];
+                r_label[r_c++] = labels[i];
+            } else {
+                l_weight[l_c] = weights[i];
+                l_labels[l_c++] = labels[i];
+            }
+        }
+    }
+    
+    for (int attr = 0; attr < max_feature; ++attr) {
+        float *l_start = sp.l.data + sp.l.line_count*attr;
+        float *r_start = sp.r.data + sp.r.line_count*attr;
+        float *o_start = data + line_count*attr;
+        
+        int l_c = 0;
+        int r_c = 0;
+        for (int i = 0; i < line_count; i ++) {
+            if (bitmap[i]) {
+                r_start[r_c++] = o_start[i];
+            } else {
+                l_start[l_c++] = o_start[i];
+            }
+        }
+        if (l_c + r_c != line_count) {
+            fprintf(stderr, "r_c(%d)+l_c(%d) not matchs line_count(%d)\n", r_c, l_c, line_count);
+            _exit(1);
+        }
+    }
+    return sp;
 }
 
 
@@ -233,17 +381,48 @@ public:
 
 void DecisionTree::dump_tree(FILE* fp, decision_tree::node* n)
 {
-    //TODO
+    for (int i = 0; i < n->level; i ++)
+        fprintf(fp, " ");
+    if (n->is_leaf) {
+        fprintf(fp, "-1(leaf) %.6g pos:%d neg:%d type:%s\n", n->value, n->ig, 
+                                                        n->pos_count, n->neg_count,
+                                                        n->pos_count > n->neg_count ? "1" : "-1");
+    } else {
+        fprintf(fp, "%d(%.6g) %.6g\n", n->attribute, n->value, n->ig);
+    }
+    
+    if (n->left) dump_tree(fp, n->left);
+    if (n->right) dump_tree(fp, n->right);
 }
 
 void DecisionTree::to_model_file(FILE* fp, node* root)
 {
-    //TODO
+    fprintf(fp, "[\n");
+    if (root->is_leaf) {
+        fprintf(fp, "attribute %d\n", -1);
+        fprintf(fp, "pos %d\n", root->pos_count);
+        fprintf(fp, "neg %d\n", root->neg_count);
+        fprintf(fp, "label %d\n", root->majority_label);
+    } else {
+        fprintf(fp, "attribute %d\n", root->attribute);
+        fprintf(fp, "value %.6g\n", root->value);
+        
+        if (root->left) to_model_file(fp, root->left);
+        if (root->right) to_model_file(fp, root->right);
+    }
+    fprintf(fp, "]\n");
 }
 
 int DecisionTree::predict(node* root, float* data, int length)
 {
-    //TODO
+    if (root->is_leaf)
+        return root->majority_label;
+    
+    assert(root->attribute >= 0 && root->attribute < length);
+    float value = data[root->attribute];
+    if (value < root->value)
+        return predict(root->left, data, length);
+    return predict(root->right, data, length);
 }
 
 int DecisionTree::predict(float* data, int length) {
