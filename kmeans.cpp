052a5a65fd09e8458c39ce3800be4e64c549402a
wangrn <wang_rn@163.com>
2013-07-26 22:42:14 +0800

    Update kmeans.cpp

--- kmeans.cpp
+++ kmeans.cpp
@@ -12,8 +12,37 @@ const int MAX_ITER_NUM = 30;    //< 最大迭代次数
 
 typdef vector<double> Tuple;    //< 存储每个样例的属性信息
 
-int SampleNumber;   //< 数据集中的样例数目
-int DimensionsNumber;      //< 每个样例的维数
+int SampleNumber;       //< 数据集中的样例数目
+int DimensionsNumber;   //< 每个样例的维数
+vector<Tuple> tuples;   //< 数据集
+
+
+//计算两个元祖的欧几里距离
+double getDistXY(const Tuple &x, const Tuple &y)
+{
+    double sum = 0;
+    for (int i = 0; i < DimensionsNumber; i ++)
+        sum += (x[i]-y[i])*(x[i]-y[i]);
+    return sqrt(sum);
+}
+
+// 
+int clusterOfTuple(Tuple means[], const Tuple &tuple)
+{
+    double dist = getDistXY(means[0], tuple);
+    int label = 0;
+    for (int i = 1; i < CLUSTER_NUM; i ++)
+    {
+        double tmp = getDistXY(means[i], tuple);
+        if (tmp < dist)
+        {
+            dist = tmp;
+            label = i;
+        }
+    }
+    return label;
+}
+
 
 int main()
 {
